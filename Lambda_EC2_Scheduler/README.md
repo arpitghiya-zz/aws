f8591052ed07211038227ef0bcb8e2bf70c73f9c
Arpit Ghiya <arpitghiya@gmail.com>
2017-10-27 16:44:09 +0530

    Adding Lambda Functions

---
+++ Lambda_EC2_Scheduler/README.md
@@ -0,0 +1,3 @@
+Helper scripts for Scheduling auto start/stop of EC2 instances in AWS.
+
+For complete process, please check - 
---
+++ Lambda_EC2_Scheduler/stop_instances.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import boto3
+
+def get_instance_name(id):
+    ec2 = boto3.resource('ec2')
+    ec2instance = ec2.Instance(id)
+    instancename = ''
+    for tags in ec2instance.tags:
+        if tags["Key"] == 'Name':
+            instancename = tags["Value"]
+    return instancename
+    
+def lambda_handler(event, context):
+    ec2client = boto3.client('ec2')
+    tagkey = 'AutoRestart'
+    tagvalue = 'True'
+    response = ec2client.describe_instances(Filters=[
+            {'Name': 'tag:'+tagkey, 'Values': [tagvalue]},
+            ])
+    instancelist = []
+    print "Auto Stop of EC2 instances"
+    for reservation in (response["Reservations"]):
+        for instance in reservation["Instances"]:
+            if instance["State"]["Name"] != "running":
+                print "Instance: {} not in running state. Skipping".format(get_instance_name(instance["InstanceId"]))
+            else:
+                instancelist.append(instance["InstanceId"])
+    
+    #stop running instances
+    if len(instancelist) >0:
+        ret = ec2client.stop_instances(DryRun=False, InstanceIds=instancelist)
+        for i in ret["StoppingInstances"]:
+            print "Stopping instance: {}".format(get_instance_name(i["InstanceId"]))
+    else:
+        print "No Instance to stop"
